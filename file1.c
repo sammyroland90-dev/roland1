21ebac82d18a9db0407b15698fd46a84d6fa5792
sammyroland90-dev <Sammyroland90@gmail.com>
2025-09-25 16:26:45 +0300

    Am making the number divisible by 11 also

--- file1.c
+++ file1.c
@@ -5,10 +5,14 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &number);
     
-    if (number % 7 == 0) {
-        printf("%d is divisible by 7\n", number);
+    if (number % 7 == 0 && number % 11 == 0) {
+        printf("%d is divisible by both 7 and 11\n", number);
+    } else if (number % 7 == 0) {
+        printf("%d is divisible by 7 but not by 11\n", number);
+    } else if (number % 11 == 0) {
+        printf("%d is divisible by 11 but not by 7\n", number);
     } else {
-        printf("%d is not divisible by 7\n", number);
+        printf("%d is not divisible by either 7 or 11\n", number);
     }
     
     return 0;
